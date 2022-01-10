d8275cbbc80f0181aacaedee29b81671f529da63
Ohdonguk <not1016@naver.com>
2022-01-11 00:57:35 +0900

    #3.4 Airbnb_clone

---
+++ users/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "gender", "currency", "superhost")
+    list_filter = ("langauge", "currency", "superhost")
