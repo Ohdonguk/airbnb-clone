a2f3f2bba102c2697bc967d5de6efaabe8f53b09
Ohdonguk <not1016@naver.com>
2022-01-14 01:57:19 +0900

    #6.0 Room admin
    Class meta:
    	abstract = True
    Abstract model은 데이터베이스에서는 나타나지 않는 model이다
    
    Relationship
    Models.ForeignKey 는 일대다 관계이고
    Models.ManyToManyField 는 다대다 관계이다
    
    on_delete=models.CASCADE <=는 user를 삭제할때 연결된 모두를 삭제한다(폭포수 => 위에서 아래로)
    on_delete는 오직 ForeignKey 를 위한 코드다.
    (더 많은 것은 Django 에서 ForeignKey 검색)
    
    Mets clss란 모델 내의 모든 class들 안에 있는 class이다 Meta class로 많은 설정을 할 수 있다.
    
    예:(verbose_name_plural <= 이름 설정 가능
          ordering <= 기본으로 설정되어 있는 순서 변경 가능)
    
    search_fields <= Django 내에 검색 기능 (기능 참조 Django admin search)

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Review)
+class ReaviewAdmin(admin.ModelAdmin):
+
+    """ Review Admin Definition """
+
+    pass
--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,46 @@
+from csv import list_dialects
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    pass
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "country",
+        "city",
+    )
+
+    search_fields = ("city", "^host__username")
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ Photo Admin Definition """
+
+    pass
