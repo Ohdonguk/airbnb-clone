ad7c58a85a563b0cb5d7c873c190cc68e8825177
Ohdonguk <not1016@naver.com>
2022-01-19 14:35:23 +0900

    #10.1 view , urls
    request의 전달과정은 다음과 같습니다.
    user -----> web_server ----> WSGI-server -----> room_app
    
    우선 user가 chrome이나 firefox와 같은 웹브라우저 를 이용하여 우리의 web_server에
    request를 보내게 됩니다.
    
    서버에서는 request를 받게되고, static 파일을 요구하는지? dynamic 파일을 요구하는지 확인 후
    static이라면 웹 서버에서 바로 처리할 것 이고,
    dynamic 이라면 WSGI-server에서 처리하게 될 것 입니다.
    
    WSGI-서버 에서는 우리의 room app에 요청을 보내고, app에서 view를 통하여 처리 및 가공을 한후, 가공된 data를 template를 활용하여 rendering 하여 사용자의 화면으로 다시 response을 보내는 것 입니다.

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from argparse import Namespace
+from django.urls import path
+from rooms import views as room_views
+
+app_name = "core"
+
+urlpatterns = [path("", room_views.all_rooms, name="home")]
