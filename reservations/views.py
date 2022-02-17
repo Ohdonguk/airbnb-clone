c829475ecd937e7d2797c6e5438590b0b29f248a
Ohdonguk <not1016@naver.com>
2022-02-17 18:13:22 +0900

    #24.9 Create Reservation

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,37 @@
+import datetime
 from django.shortcuts import render
+from rooms import models as room_models
+from django.views.generic import View
+from . import models
+from django.contrib import messages
+from django.shortcuts import redirect, reverse
 
-# Create your views here.
+import reservations
+
+
+class CreateError(Exception):
+    pass
+
+
+def create(request, room, year, month, day):
+    try:
+        date_obj = datetime.datetime(year, month, day)
+        room = room_models.Room.objects.get(pk=room)
+        models.BookedDay.objects.get(day=date_obj, reservation__room=room)
+        raise CreateError()
+    except (room_models.Room.DoesNotExist, CreateError):
+        messages.error(request, "Can't Reserve Teat Room")
+        return redirect(reverse("core:home"))
+    except models.BookedDay.DoesNotExist:
+        reservation = models.Reservation.objects.create(
+            guest=request.user,
+            room=room,
+            check_in=date_obj,
+            check_out=date_obj + datetime.timedelta(days=1),
+        )
+        return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
+
+
+class ReservationDetail(View):
+    def get(self):
+        pass
