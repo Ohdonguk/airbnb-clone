423c3c73f1599e74331bc9fd2adc30a3ae870140
Ohdonguk <not1016@naver.com>
2022-02-23 13:28:45 +0900

    #26.3 Postgres part One

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,6 @@ DEBUG = bool(os.environ.get("DEBUG"))
 
 ALLOWED_HOSTS = []
 
-print(DEBUG)
 
 # Application definition
 
@@ -99,7 +98,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            "HOST": os.environ.get("RDS_HOST"),
+            "NAME": os.environ.get("RDS_NAME"),
+            "USER": os.environ.get("RDS_USER"),
+            "PASSWORD": os.environ.get("RDS_PASSWORD"),
+            "PORT": "5432",
         }
     }
 
